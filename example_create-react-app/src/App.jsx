65bcda5b422ead920b4bb1ae8db1c1afad06c1e3
Taylor Abraham <taylor.abra8@gmail.com>
2020-04-10 15:35:17 -0400

    Add base create-react-app example code

---
+++ example_create-react-app/src/App.jsx
@@ -0,0 +1,60 @@
+import React, { useState, Suspense, lazy } from 'react'
+import { render } from 'react-dom'
+
+const CyBuddy = lazy(() => import('@karimsa/cybuddy'))
+
+import('jquery').then(($) => {
+	window.$ = $
+})
+
+export default function App() {
+	if (
+		process.env.NODE_ENV !== 'production' &&
+		new URLSearchParams(window.location.search).get('testMode') === 'true'
+	) {
+		return (
+			<Suspense fallback={<p>Loading ...</p>}>
+				<CyBuddy
+					baseURL="http://localhost:1234"
+					verifyTestMode={() =>
+						Promise.resolve(process.env.NODE_ENV === 'test')
+					}
+					initialSteps={[
+						{
+							action: 'reset',
+						},
+					]}
+				/>
+			</Suspense>
+		)
+	}
+
+	return <Home />
+}
+
+function Home() {
+	const [counter, setCounter] = useState(0)
+	return (
+		<React.Fragment>
+			<h1>Hello world</h1>
+			<p>This is a paragraph</p>
+			<p>Counter: {counter}</p>
+			<button
+				data-test="btn-decrease"
+				type="button"
+				onClick={() => setCounter(counter - 1)}
+			>
+				-
+			</button>
+			<button
+				data-test="btn-increase"
+				type="button"
+				onClick={() => setCounter(counter + 1)}
+			>
+				+
+			</button>
+		</React.Fragment>
+	)
+}
+
+render(<App />, document.getElementById('app'))
